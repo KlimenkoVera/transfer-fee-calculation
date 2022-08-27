879c582c62b31e84d6a92d4a970d8470820851b2
KlimenkoVera <klimenkovera1991@gmail.com>
2022-08-27 13:23:09 +0300

    Расчет комиссии

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,4 +1,12 @@
 package ru.netology
 
-class Main {
+fun main () {
+    val amount = 2_000_00
+    val transferFee = 75 
+    val feeStart = 4_666_67
+    val minFee = 35_00
+
+    val totalFee = if (amount > feeStart) amount * transferFee/10000 else minFee
+
+    println("Ваша комиссия за перевод составит: $totalFee")
 }
