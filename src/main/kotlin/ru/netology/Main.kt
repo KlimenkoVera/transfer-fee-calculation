6eed52055df01dedbc2966f815ad5d68a1d7679a
KlimenkoVera <klimenkovera1991@gmail.com>
2022-08-28 22:05:32 +0300

    "Разная комиссия"

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,12 +1,38 @@
 package ru.netology
 
+enum class TypeCard {
+    Mastercard, Maestro, Visa, Mir, VkPay
+}
+
+const val MIN_FEE = 35_00
+const val LIMIT = 75_000_00
+
 fun main () {
-    val amount = 2_000_00
-    val transferFee = 75 
-    val feeStart = 4_666_67
-    val minFee = 35_00
+val result = totalFee (amountOfTransfer = 2_000_00, amountOfTransferPerMonth = 75_000_00, typeCard = TypeCard.Maestro)
 
-    val totalFee = if (amount > feeStart) amount * transferFee/10000 else minFee
 
-    println("Ваша комиссия за перевод составит: $totalFee")
+    println("Ваша комиссия за перевод составит: $result копеек")
+}
+
+fun userCardToFee (
+    typeCard: TypeCard = TypeCard.VkPay
+): Double = when (typeCard) {
+    TypeCard.VkPay -> .0
+    TypeCard.Mir, TypeCard.Visa -> .0075
+    TypeCard.Maestro, TypeCard.Mastercard -> .006
+}
+
+fun totalFee (
+    amountOfTransfer: Int,
+    amountOfTransferPerMonth: Int = 0,
+    typeCard: TypeCard = TypeCard.VkPay
+): Int {
+val fee = (userCardToFee(typeCard) * amountOfTransfer).toInt()
+val totalPay = amountOfTransfer + amountOfTransferPerMonth
+  return  when (typeCard) {
+        TypeCard.VkPay -> fee
+        TypeCard.Mir, TypeCard.Visa -> if (fee < MIN_FEE) MIN_FEE else fee
+        TypeCard.Mastercard,  TypeCard.Maestro -> if (totalPay > LIMIT) fee else 0
+    }
+
 }
