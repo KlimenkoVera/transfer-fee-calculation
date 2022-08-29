5aa1647f5b2c6d3ddda27ef020f3cd0088ca81c4
KlimenkoVera <klimenkovera1991@gmail.com>
2022-08-29 23:20:10 +0300

    Подключение jacoco

--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -1,6 +1,5 @@
 package ru.netology
 
-import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -8,25 +7,10 @@ import org.junit.Assert.*
 class MainKtTest {
 
     @Test
-    fun main() {
-        val transfer = 10_000_00
-        val typeCard: TypeCard = TypeCard.Mastercard
-        val transferPerMonth = 80_000_00
-
-        val result = totalFee(
-            amountOfTransfer = transfer,
-            typeCard = typeCard,
-            amountOfTransferPerMonth = transferPerMonth
-        )
-
-        assertEquals(60_00, result)
-    }
-
-    @Test
     fun totalFee_forVkPay() {
         val transfer = 10_000_00
         val typeCard: TypeCard = TypeCard.VkPay
-        val transferPerMonth = 80_000_00
+        val transferPerMonth = 0
 
         val result = totalFee(
             amountOfTransfer = transfer,
@@ -55,7 +39,7 @@ class MainKtTest {
     @Test
     fun totalFee_forVisaOrMirIfTrue() {
         val transfer = 2_000_00
-        val typeCard: TypeCard = TypeCard.Mir
+        val typeCard: TypeCard = TypeCard.Visa
         val transferPerMonth = 80_000_00
 
         val result = totalFee(
@@ -83,9 +67,9 @@ class MainKtTest {
     }
 
     @Test
-    fun totalFee_forMastercardOrMaestroIfFalsee() {
+    fun totalFee_forMastercardOrMaestroIfFalse() {
         val transfer = 10_000_00
-        val typeCard: TypeCard = TypeCard.Mastercard
+        val typeCard: TypeCard = TypeCard.Maestro
         val transferPerMonth = 10_000_00
 
         val result = totalFee(
@@ -100,9 +84,10 @@ class MainKtTest {
     @Test
     fun userCardToFee_forVkPay() {
 
+        val typeCard: TypeCard = TypeCard.VkPay
         val expectedResult = .0
 
-        val result = userCardToFee(typeCard = TypeCard.VkPay)
+        val result = userCardToFee(typeCard = typeCard)
 
         assertEquals(expectedResult, result, 0.0)
     }
@@ -110,20 +95,24 @@ class MainKtTest {
     @Test
     fun userCardToFee_forVisaOrMir() {
 
+        val typeCard: TypeCard = TypeCard.Visa
         val expectedResult = .0075
 
-        val result = userCardToFee(typeCard = TypeCard.Visa)
+        val result = userCardToFee(typeCard = typeCard)
 
         assertEquals(expectedResult, result, 0.0)
     }
+
 
     @Test
     fun userCardToFee_forMastercardOrMaestro() {
 
+        val typeCard: TypeCard = TypeCard.Mastercard
         val expectedResult = .006
 
-        val result = userCardToFee(typeCard = TypeCard.Mastercard)
+        val result = userCardToFee(typeCard = typeCard)
 
         assertEquals(expectedResult, result, 0.0)
     }
+
 }
